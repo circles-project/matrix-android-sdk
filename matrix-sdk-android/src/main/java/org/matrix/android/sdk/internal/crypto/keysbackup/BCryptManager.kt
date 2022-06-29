250c7d623cf4f38e6e2d5b7839e806c5980b593b
Taras <tarassmakula@gmail.com>
2022-06-29 17:27:06 +0300

    Add key length

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/crypto/keysbackup/BCryptManager.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/crypto/keysbackup/BCryptManager.kt
@@ -7,21 +7,28 @@ internal object BCryptManager {
 
     private const val iterations = 14
     private const val saltLength = 16
+    private const val privateKeyLength = 32
 
     fun generateBcryptPrivateKeyWithPassword(
         userName: String,
         password: String
     ): GeneratePrivateKeyResult {
-        val salt = userName.sha256().substring(0, saltLength).toByteArray()
-        val privateKey = BCrypt.withDefaults().hash(iterations, salt, password.toByteArray())
-        return GeneratePrivateKeyResult(privateKey, salt.toString(), iterations)
+        val salt = userName.sha256().substring(0, saltLength)
+        val privateKey = retrievePrivateKeyWithPassword(password, salt, iterations)
+        return GeneratePrivateKeyResult(privateKey, salt, iterations)
     }
 
     fun retrievePrivateKeyWithPassword(
         password: String,
-        salt: String,
-        iterations: Int
-    ): ByteArray = BCrypt.withDefaults().hash(iterations, salt.toByteArray(), password.toByteArray())
+        salt: String?,
+        iterations: Int?
+    ): ByteArray =
+        BCrypt.withDefaults()
+            .hash(iterations ?: this.iterations, (salt ?: "").toByteArray(), password.toByteArray())
+            .takeLast(privateKeyLength)
+            .reversed()
+            .toByteArray()
+
 
     private fun String.sha256(): String = MessageDigest
         .getInstance("SHA-256")
