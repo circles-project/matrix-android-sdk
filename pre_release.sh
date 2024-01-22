7b12bcf6fba94eeb5e6e59d7c366557e25c4a664
Taras Smakula <tarassmakula@gmail.com>
2024-01-22 15:51:10 +0200

    script changes

--- pre_release.sh
+++ pre_release.sh
@@ -9,19 +9,20 @@ version_name=$(grep '^VERSION_NAME=' gradle.properties | cut -d'=' -f2)
 echo "Current version name: $version_name"
 
 # Change version in build.gradle
-awk -v version="$version_name" '/version =/ {$3 = "\"" version "\""} 1' build.gradle > tmp && mv tmp build.gradle
+awk -v version="$version_name" '/version =/ {gsub(/".*"/, "\"" version "\"")} 1' matrix-sdk-android/build.gradle > tmp && mv tmp matrix-sdk-android/build.gradle
 
 # Change version in jitpack.yml
-awk -v version="$version_name" '/-Dversion=/ {$2 = version} 1' jitpack.yml > tmp && mv tmp jitpack.yml
+awk -v version="$version_name" '/-Dversion=/ {gsub(/=.*/, "= " version)} 1' jitpack.yml > tmp && mv tmp jitpack.yml
 
 # Build the project
+./gradlew clean
 ./gradlew assembleRelease
-
-# Move matrix-sdk-android-release.aar to the root directory
-mv matrix-sdk-android/build/outputs/aar/matrix-sdk-android-release.aar matrix-sdk-android-release.aar
 
 # Generate POM file for release publication
 ./gradlew :matrix-sdk-android:generatePomFileForReleasePublication
+
+# Move matrix-sdk-android-release.aar to the root directory
+mv matrix-sdk-android/build/outputs/aar/matrix-sdk-android-release.aar matrix-sdk-android-release.aar
 
 # Move pom-default.xml to the root directory and rename it to pom.xml
 mv matrix-sdk-android/build/publications/release/pom-default.xml pom.xml
