ac0c815994bbea486d43d82560c789db1dabf98b
Taras Smakula <tarassmakula@gmail.com>
2024-01-22 15:28:43 +0200

    Change sed commands

--- pre_release.sh
+++ pre_release.sh
@@ -9,10 +9,10 @@ version_name=$(grep '^VERSION_NAME=' gradle.properties | cut -d'=' -f2)
 echo "Current version name: $version_name"
 
 # Change version in build.gradle
-sed -i "s/version = \".*\"/version = \"$version_name\"/" build.gradle
+awk -v version="$version_name" '/version =/ {$3 = "\"" version "\""} 1' build.gradle > tmp && mv tmp build.gradle
 
 # Change version in jitpack.yml
-sed -i "s/-Dversion=.*/-Dversion=$version_name/" jitpack.yml
+awk -v version="$version_name" '/-Dversion=/ {$2 = version} 1' jitpack.yml > tmp && mv tmp jitpack.yml
 
 # Build the project
 ./gradlew assembleRelease
@@ -27,4 +27,5 @@ mv matrix-sdk-android/build/outputs/aar/matrix-sdk-android-release.aar matrix-sd
 mv matrix-sdk-android/build/publications/release/pom-default.xml pom.xml
 
 # Remove <packaging>aar</packaging> from pom.xml
-sed -i '/<packaging>aar<\/packaging>/d' pom.xml
+awk '!/<packaging>aar<\/packaging>/' pom.xml > tmp && mv tmp pom.xml
+
