e0904b9910afb47883f1a2aabc1dfebb2b95e171
Taras Smakula <tarassmakula@gmail.com>
2024-06-18 16:31:21 +0300

    v1.6.10.42

--- matrix-sdk-android/build.gradle.kts
+++ matrix-sdk-android/build.gradle.kts
@@ -206,7 +206,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "org.futo.gitlab.circles"
             artifactId = "matrix-android-sdk"
-            version = "1.6.10.41"
+            version = "1.6.10.42"
 
             afterEvaluate {
                 from(components["release"])
